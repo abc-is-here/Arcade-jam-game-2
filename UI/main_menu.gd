df31b620fd3b809209c6466fd0c8d8e94aa06999
abc <abhaygupta.hsj@gmail.com>
2024-08-12 21:22:16 +0530

    Made better UI

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,2 +1,18 @@
+class_name Mainmenu
 extends Control
 
+@onready var play_button = $MarginContainer/HBoxContainer/VBoxContainer/PlayButton as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
+@onready var start_level = preload("res://world.tscn") as PackedScene
+@onready var options = $Options
+@onready var margin_container = $MarginContainer
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_packed(start_level)
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+func _on_option_button_pressed():
+	options.visible = true
+	margin_container.visible = false
--- UI/options.gd
+++ UI/options.gd
@@ -1,18 +1,5 @@
 class_name OptionsMenu
 extends Control
 
-@onready var exit_button = $MarginContainer/VBoxContainer/Exit_Button as Button
-
-signal exit_options_menu
-
-func _ready():
-	exit_button.button_down.connect(on_exit_pressed)
-	set_process(false)
-
-func on_exit_pressed() -> void:
-	exit_options_menu.emit()
-	set_process(false)
-
-
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file('res://UI/main_menu.tscn')
