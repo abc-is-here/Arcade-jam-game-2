27ad930ed258173e2425890b871cb3721a4a5028
abc <abhaygupta.hsj@gmail.com>
2024-08-14 15:40:01 +0530

    Completed the rebounding of keys!

--- UI/hotkey_rebind_button.gd
+++ UI/hotkey_rebind_button.gd
@@ -7,6 +7,15 @@ var is_remapping = false
 var action_to_remap = null
 var remapping_button = null
 
+var input_actions = {
+	"up": "Move Up",
+	"down": "Move Down",
+	"left": "Move Left",
+	"right": "Move Right",
+	"jump": "Jump",
+	"sprint": "sprint",
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_create_action_list()
@@ -16,22 +25,52 @@ func _create_action_list():
 	for item in list_actions.get_children():
 		item.queue_free()
 		
-	for action in InputMap.get_actions():
+	for action in input_actions:
 		var button = input_button.instantiate()
 		var action_label = button.find_child("LabelAction")
 		var input_label = button.find_child("LabelInput")
 		
-		action_label.text = action
+		action_label.text = input_actions[action]
 		
 		var events = InputMap.action_get_events(action)
 		if events.size() > 0:
-			input_label.text = events[0].as_text()
+			input_label.text = events[0].as_text().trim_suffix(" (Physical)")
 		else:
 			input_label.text = ""
 		
 		list_actions.add_child(button)
+		button.pressed.connect(_on_input_button_pressed.bind(button, action))
+		
+func _on_input_button_pressed(button, action):
+	if !is_remapping:
+		is_remapping = true
+		action_to_remap = action
+		remapping_button = button
+		button.find_child("LabelInput").text = "Press key to bind..."
+
+func _input(event):
+	if is_remapping:
+		if (
+			event is InputEventKey ||
+			(event is InputEventMouseButton && event.pressed)
+		):
+			if event is InputEventMouseButton && event.double_click:
+				event.double_click = false
+			
+			
+			InputMap.action_erase_events(action_to_remap)
+			InputMap.action_add_event(action_to_remap, event)
+			_update_action_list(remapping_button, event)
+			
+			is_remapping = false
+			action_to_remap = null
+			remapping_button = null
+			
+			accept_event()
+			
+func _update_action_list(button, event):
+	button.find_child("LabelInput").text = event.as_text().trim_suffix(" (Physical)")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_reset_button_pressed():
+	_create_action_list()
