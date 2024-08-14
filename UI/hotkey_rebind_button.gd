8f974871f312894852f4d1d2b9bf6e78fbb21851
abc <abhaygupta.hsj@gmail.com>
2024-08-14 12:19:25 +0530

    Made the control rebind option buttons but size is not correct

--- UI/hotkey_rebind_button.gd
+++ UI/hotkey_rebind_button.gd
@@ -1,14 +1,35 @@
 extends Control
 
-@onready var label = $HBoxContainer/Label as Label
-@onready var button = $HBoxContainer/Button as Button
+@onready var input_button = preload("res://UI/input_button.tscn")
+@onready var list_actions = $PanelContainer/MarginContainer/VBoxContainer/ScrollContainer/listActions
 
-@export var action_name: String = "left"
-
+var is_remapping = false
+var action_to_remap = null
+var remapping_button = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_create_action_list()
+	
+func _create_action_list():
+	InputMap.load_from_project_settings()
+	for item in list_actions.get_children():
+		item.queue_free()
+		
+	for action in InputMap.get_actions():
+		var button = input_button.instantiate()
+		var action_label = button.find_child("LabelAction")
+		var input_label = button.find_child("LabelInput")
+		
+		action_label.text = action
+		
+		var events = InputMap.action_get_events(action)
+		if events.size() > 0:
+			input_label.text = events[0].as_text()
+		else:
+			input_label.text = ""
+		
+		list_actions.add_child(button)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
