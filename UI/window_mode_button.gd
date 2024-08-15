e6113d5baacf9b869ee4bdf98319c968d1d81907
abc <abhaygupta.hsj@gmail.com>
2024-08-15 06:55:24 +0530

    Now the resolution settings work!!!

---
+++ UI/window_mode_button.gd
@@ -0,0 +1,43 @@
+extends Control
+
+
+@onready var option_button = $HBoxContainer/OptionButton as Button
+
+
+const Window_mode_array: Array[String] = [
+	"Full-Screen",
+	"Window-Mode",
+	"Borderless-Window",
+	"Borderless Full-Screen"
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_window_mode_items()
+	option_button.item_selected.connect(on_window_mode_selected)
+	
+func add_window_mode_items() -> void:
+	for windowMode in Window_mode_array:
+		option_button.add_item(windowMode)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func on_window_mode_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+
+		2:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			
+		3:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
