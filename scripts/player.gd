0bba180db26d030fa456224e5ac3b495529cc7a1
abc <abhaygupta.hsj@gmail.com>
2024-08-18 06:25:15 +0530

    Added a plane instead of that ground and more!

--- scripts/player.gd
+++ scripts/player.gd
@@ -46,7 +46,7 @@ func _physics_process(delta):
 	else:
 		speed = WALK_SPEED
 
-	var input_dir = Input.get_vector("right", "left", "down", "up")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	if is_on_floor():
