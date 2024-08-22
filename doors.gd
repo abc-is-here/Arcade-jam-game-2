8800b6b9be1961b0da2031d26dc591e62805c2f8
abc <abhaygupta.hsj@gmail.com>
2024-08-22 08:46:25 +0530

    Enemy and what not!

--- doors.gd
+++ doors.gd
@@ -1,6 +1,11 @@
 extends Area3D
 
+# Assuming the player is an autoload singleton
+func _on_area3d_body_entered(body):
+	if body.name == "player":  # Ensure it's the player entering the Area3D
+		body.saved_position = body.global_transform.origin
+		change_scene()
 
-func _on_body_entered(body):
-	if body.name == 'player':
-		print(body.position)
+func change_scene():
+	var room1_scene = preload("res://room1.tscn")
+	get_tree().change_scene_to(room1_scene)
--- world.gd
+++ world.gd
@@ -1,4 +1,8 @@
 extends Node3D
 
+@onready var player: CharacterBody3D = $player
+
+
+
 func _ready():
 	$Fade_animations/AnimationPlayer.play("fade_out")
